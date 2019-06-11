afe5f4c6aea6c343d39cc2a5c0671e3dbbdd8cad
cesar daniel <cdanielmg200@gmail.com>
2019-06-11 14:02:50 -0500

    Estructura base del proyecto

---
+++ src/main/java/com/example/service/person/PersonController.java
@@ -0,0 +1,9 @@
+package com.example.service.person;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+}
